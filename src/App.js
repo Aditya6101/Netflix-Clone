c4326f7209955084caec9aa7b773a8b73dd5879a
Aditya Kamble <adityakamble6101@gmail.com>
2021-08-03 12:30:36 +0530

    Movie Rows Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import Row from "./components/Row";
+import requests from "./requests";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
+      <Row
+        title="Netfilx Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/components/Row.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+
+import "./Row.css";
+
+const basePosterUrl = "https://image.tmdb.org/t/p/original";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await axios.get(fetchUrl);
+      setMovies(res.data.results);
+      console.log(res);
+    };
+    fetchMovies();
+  }, [fetchUrl]);
+  console.log(movies);
+
+  return (
+    <div>
+      <h3> {title} </h3>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__poster-large"}`}
+            src={`${basePosterUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
